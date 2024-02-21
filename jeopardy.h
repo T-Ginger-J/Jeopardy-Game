a3f91184512ad78badcc99caa294cba48a2d3432
William Zdenek Chamberlain <william.chamberlain@ontariotechu.net>
2024-02-20 23:23:21 -0500

    Added group members names and ids,
    Added group members names and ids.
    Updated copyright year.

--- jeopardy.h
+++ jeopardy.h
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2024, <Trent Jordan (100831853)>, <Loona Khan (100782789)>, <William Chamberlain (100846922)>
  * All rights reserved.
  *
  */
