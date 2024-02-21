25a6b0afb6cce069ef05b5ecb5bf3a758ecdb9ae
William Zdenek Chamberlain <william.chamberlain@ontariotechu.net>
2024-02-20 21:39:47 -0500

    Players.c completed and minimized,
    Players.c and accosiated .h have been completed and an additional function to check for player existence has been added.
    Requirement for number of players integer has been removed from the functions.

--- players.c
+++ players.c
@@ -8,27 +8,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "assert.h"
 #include "players.h"
 
-// Returns true if the player name matches one of the existing players
-bool player_exists(player *players, int num_players, char *name)
-{
-    bool found = false;
-    for (int i = 0; !found && i < sizeof(players) / sizeof(player); i++) {
-        found = strcmp(players[i].name, name) == 0;
+// Returns the index of the player with given name, returns -1 otherwise
+int player_index(player *players, char *name) {
+    for (int i = 0; i < sizeof(players) / sizeof(player); i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            return i;
+        }
     }
-    return found;
+    return -1;
+}
+
+// Returns true if the player name matches one of the existing players
+bool player_exists(player *players, char *name) {
+    return player_index(players, name) != -1;
 }
 
 // Go through the list of players and update the score for the 
 // player given their name
-void update_score(player *players, int num_players, char *name, int score)
+void update_score(player *players, char *name, int score)
 {
-    assert(player_exists(players, num_players, name));
-    int playerind = 0;
-    while(strcmp(players[playerind].name, name) != 0) {
-        playerind++;
-    }
-    players[playerind].score += score;
+    assert(player_exists(players, name));
+    players[player_index(players, name)].score += score;
 }
--- players.h
+++ players.h
@@ -18,11 +18,14 @@ typedef struct {
     int score;
 } player;
 
+// Returns the index of the player with the accosiated name
+extern int player_index(player *players, char *name);
+
 // Returns true if the player name matches one of the existing players
-extern bool player_exists(player *players, int num_players, char *name);
+extern bool player_exists(player *players, char *name);
 
 // Go through the list of players and update the score for the 
 // player given their name
-extern void update_score(player *players, int num_players, char *name, int score);
+extern void update_score(player *players, char *name, int score);
 
 #endif /* PLAYERS_H_ */
